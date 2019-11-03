3c27960c35f5d6fb97dfb4a8e40a5a59ca450fe8
EC2 Default User <ec2-user@ip-172-31-42-25.us-east-2.compute.internal>
2019-11-03 22:18:38 +0000

    more formatting

--- js/hw2.js
+++ js/hw2.js
@@ -1,1 +1,13 @@
 //Javascript File
+function isFormValid(){
+    let isValid = true;
+    if ($("#q1").val() == "") {
+        isValid = false;
+        $("#validationFdbk").html("Question 1 was not answered");
+    }
+    else if($("#q8").val() =="") {
+        isValid = false;
+        $("#validationFdbk").html("Question 8 was not answered");
+    }
+    return isValid;
+}
